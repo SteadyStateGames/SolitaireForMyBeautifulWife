28ab861999d5d386bd47eceed9db4ff508c57bc4
Steady State Games <steadystategames@gmail.com>
2026-07-31 12:44:24 -0600

    Landscape reclaims the stock band, readable fans under squeeze, dealt-row size fix

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1785522318|3411240';
+const CACHE_VERSION = '1785523454|3733186';
 /** @type {string} */
 const CACHE_PREFIX = 'Spider Solitaire-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
