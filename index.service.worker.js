5f50b8799a65359e6cc3784ca2e7746409c960c5
Steady State Games <steadystategames@gmail.com>
2026-07-31 11:50:55 -0600

    Clean rebuild: exclude export folders from resource scan

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1785520062|3345403';
+const CACHE_VERSION = '1785520239|3472779';
 /** @type {string} */
 const CACHE_PREFIX = 'Spider Solitaire-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
