8f105e56b302cfc4758912b192c9c66e3f9cf4df
Steady State Games <steadystategames@gmail.com>
2026-07-31 11:47:52 -0600

    Simple card faces by default, with a Classic toggle in Options

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1785473325|3149629';
+const CACHE_VERSION = '1785520062|3345403';
 /** @type {string} */
 const CACHE_PREFIX = 'Spider Solitaire-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
