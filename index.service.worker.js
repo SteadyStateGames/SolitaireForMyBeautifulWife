9c091afc9dab660ac6fad0c906917ac0e67486af
Steady State Games <steadystategames@gmail.com>
2026-08-10 20:48:30 -0600

    Perfect royals: one rank font across all 52 cards, royals show their suit

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1786414462|3894160';
+const CACHE_VERSION = '1786416500|3119679';
 /** @type {string} */
 const CACHE_PREFIX = 'Spider Solitaire-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
