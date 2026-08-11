c4610bd331a097f5a31a60853cadbcae11c6516a
Steady State Games <steadystategames@gmail.com>
2026-08-10 20:14:36 -0600

    Perfect deck (default), poker-felt background, Undo All confirmation gate

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1785532149|3602021';
+const CACHE_VERSION = '1786414462|3894160';
 /** @type {string} */
 const CACHE_PREFIX = 'Spider Solitaire-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
