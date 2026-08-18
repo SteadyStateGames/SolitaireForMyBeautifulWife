dd88ee311e1369a5a6860a8e39859795ec86455f
Steady State Games <steadystategames@gmail.com>
2026-08-17 20:36:16 -0600

    Fix web crash: felt grain no longer needs the noise module

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1786418291|5213819';
+const CACHE_VERSION = '1787020403|37279487';
 /** @type {string} */
 const CACHE_PREFIX = 'Spider Solitaire-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
