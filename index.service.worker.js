5e593d8940bb6ebc341b21a58cee8a5b91396512
Steady State Games <steadystategames@gmail.com>
2026-09-15 03:06:24 -0600

    Publish five-step difficulty, simple points, and 500-point gold rewards

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1789461056|3618151';
+const CACHE_VERSION = '1789463061|3242744';
 /** @type {string} */
 const CACHE_PREFIX = 'Spider Solitaire-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
