8cb103fdfa62a78e4640235da3ecfd1127965805
Steady State Games <steadystategames@gmail.com>
2026-09-17 22:33:25 -0600

    Publish accidental New deal protection

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1789702895|4281969';
+const CACHE_VERSION = '1789706003|3419093';
 /** @type {string} */
 const CACHE_PREFIX = 'Spider Solitaire-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
