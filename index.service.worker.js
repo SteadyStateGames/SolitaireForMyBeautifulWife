02688b744ba7c897073f3b7a4dea3f6c45ca013c
Steady State Games <steadystategames@gmail.com>
2026-09-19 10:46:14 -0600

    Publish distinct FreeCell slot groups

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1789835794|4398697';
+const CACHE_VERSION = '1789836372|4175469';
 /** @type {string} */
 const CACHE_PREFIX = 'Spider Solitaire-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
